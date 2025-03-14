c89fda74223ef980e050d61c106e9454829336be
Frankie <emcredmond@smith.edu>
2025-03-14 13:39:42 -0400

    update

--- app/src/main/java/a4/Main.java
+++ app/src/main/java/a4/Main.java
@@ -1,9 +1,14 @@
 package a4;
 
+import java.util.ArrayDeque;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Calls from the command line:");
-    System.out.println("    java Postfix <postfix-expr>");
+    System.out.println("    java Postfix <3 2 + 5 *>");
+    System.out.println(Tokenizer.readTokens("3 2 +"));
+    ArrayDeque<Object> tokens= Tokenizer.readTokens("3 2 +");
+    System.out.println(Postfix.postfix(tokens));
     System.out.println("    java Calculate <infix-expr>");
   }
 }
--- app/src/main/java/a4/Postfix.java
+++ app/src/main/java/a4/Postfix.java
@@ -4,7 +4,34 @@ import java.util.ArrayDeque;
 
 public class Postfix {
 
-    public static Double postfix(ArrayDeque<Object> tokens) {
+    public static boolean postfix(ArrayDeque<Object> tokens) {
+        ArrayDeque <Double> stack= new ArrayDeque<>();
+        if (tokens.peek() instanceof Double){
+            Double toAddStack= (Double)tokens.removeFirst();
+            stack.addFirst(toAddStack);
+            System.out.println(stack);
+        }
+        tokens.removeFirst();
+        return (tokens.peekFirst() instanceof Character);
+            // Double firstElem= stack.removeFirst();
+            // Double secondElem= stack.removeFirst();
+            // Character operator= (Character) tokens.removeFirst();
+            // if (operator== '*'){
+            //     Double result=secondElem*firstElem;
+            //     stack.addFirst(result);
+            // }
+            // else if (operator== '+'){
+            //     Double result=secondElem+firstElem;
+            //     return result;
+            // }
+            // else if (operator== '-'){
+            //     Double result=secondElem-firstElem;
+            //     stack.addFirst(result);
+            // }
+            // else if (operator== '/'){
+            //     Double result=secondElem/firstElem;
+            //     stack.addFirst(result);
+            // }
+            // else{}
+        }
     }
-    
-}
